e0cd95b0f121895db929a657dff5fa6c7cda01a3
triciamedina <tricia.medina@gmail.com>
2019-10-28 15:02:53 -0700

    fix database url in config

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
-    DATABASE_URL: process.env.DB_URL || 'postgresql://dunder_mifflin@localhost/bookmarks',
+    DATABASE_URL: process.env.DATABASE_URL || 'postgresql://dunder_mifflin@localhost/bookmarks',
     TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://dunder_mifflin@localhost/bookmarks'
 }
